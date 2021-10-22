3a4698d4ed9fb2d68f9484fbaa9aa3b77369f338
Jimmy228676 <2286767479@qq.com>
2021-10-22 17:06:43 +0800

    finish transform level-2 homework

--- training/transform/transform.cpp
+++ training/transform/transform.cpp
@@ -1,40 +1,57 @@
 #include <iostream>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/calib3d.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace std;
 
-void doPerspectiveTransform( Mat input, Mat& output ) {
-    std::vector<Point2f> srcQuad( 4 ), dstQuad( 4 );
+// 回调函数 of setMouseCallback function
+void getClick(int event, int x, int y, int flags, void* srcQuad)
+{
+    // when press down the mouse
+    if(event==EVENT_LBUTTONDOWN)
+    {
+        // copy the coordinate of the mouse to the srcQuad
+        vector< Point2f >* pointSet = (vector< Point2f >*) srcQuad;
+        pointSet->push_back(Point2f(x, y));
+        cout << "get a point" << endl;
+    }
+}
+
+void doPerspectiveTransform( Mat& input, Mat& output ) {
+    // srcQuad: points of the picture
+    // dstQuad: points of the real world
+    std::vector<Point2f> srcQuad, dstQuad( 4 );
     output = input.clone();
-    srcQuad[2].x = 2265, srcQuad[3].x =  3133 , srcQuad[0].x = 2273, srcQuad[1].x = 3141;
-    srcQuad[2].y = 1325, srcQuad[3].y = 1101, srcQuad[0].y = 857, srcQuad[1].y = 709;
-    dstQuad[2].x = 0, dstQuad[3].x = 1200, dstQuad[0].x = 0, dstQuad[1].x = 1200;
-    dstQuad[2].y = 400, dstQuad[3].y = 400, dstQuad[0].y = 0, dstQuad[1].y = 0;
+    dstQuad[0] = Point2f(0, 0);
+    dstQuad[1] = Point2f(400, 0);
+    dstQuad[2] = Point2f(0, 100);
+    dstQuad[3] = Point2f(400, 100);
 
-    Mat warp_matrix = getPerspectiveTransform( srcQuad, dstQuad );
+    namedWindow( "input" );
+    imshow( "input", input );
 
-    warpPerspective( input, output, warp_matrix, Size( 1200, 400 ) );
+    // get input through the mouse
+    setMouseCallback("input", getClick, &srcQuad);
+    waitKey(0);
+
+    assert(srcQuad.size() == 4);
+
+    // 变换矩阵
+    Mat warp_matrix = getPerspectiveTransform(srcQuad, dstQuad);
+    // 根据变换矩阵生成变换后图像
+    warpPerspective( input, output, warp_matrix, Size( 400, 100));
 }
 
 
 int main() {
-    Mat img = imread(PROJECT_DIR"/car.jfif");
+    Mat img = imread(PROJECT_DIR"/car.jpg");
     Mat result;
 
     doPerspectiveTransform( img, result );
 
-    namedWindow( "input" );
     namedWindow( "output" );
-
-    // imshow( "input", img );
     imshow( "output", result );
 
     waitKey( 0 );
-
-    destroyWindow( "input" );
-    destroyWindow( "output" );
     return 0;
 }
